e8e236ea01102b25b9de965cd4511fed33da7623
DonCri <36209368+DonCri@users.noreply.github.com>
2018-05-31 17:29:49 +0200

    Add files via upload

--- Device/module.php
+++ Device/module.php
@@ -52,17 +52,8 @@
         IPS_LogMessage("ReceiveData", utf8_decode($data->Buffer));
 
         // Datenverarbeitung und schreiben der Werte in die Statusvariablen
-        $this->SendDebug("BufferIn", print_r($data->Values, true), 0);
 
-        switch($data->Values->Command){
-          case 1:
-            SetValue($this->GetIDforIdent("eGateCommand"), true);
-          break;
-
-          case 2:
-            SetValue($this->GetIDforIdent("eGateCommand"), false);
-          break;
-        }
+        
 
       }
 
