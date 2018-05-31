1b608534d5d01d71e2b427b1ee872106f41f938c
DonCri <36209368+DonCri@users.noreply.github.com>
2018-05-31 16:42:49 +0200

    Add files via upload

--- Device/module.php
+++ Device/module.php
@@ -54,7 +54,7 @@
         // Datenverarbeitung und schreiben der Werte in die Statusvariablen
         SetValue($this->GetIDForIdent("eGateData"), $data->Buffer);
 
-        $this->SendDebug("BufferIn", print_r($data->Values, true), 0);
+        $this->SendDebug("eGateDatenFilter", print_r($data->Values, true), 0);
 
         switch($data->Values->Command)
         {
