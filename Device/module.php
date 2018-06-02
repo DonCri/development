4e3087074f47bb0cb06e9be58724e043543d7c0d
DonCri <36209368+DonCri@users.noreply.github.com>
2018-06-03 01:13:49 +0200

    Add files via upload

--- Device/module.php
+++ Device/module.php
@@ -64,7 +64,7 @@ class Device extends IPSModule {
           //Neuen Wert in die Statusvariable schreiben
           SetValue($this->GetIDForIdent($Ident), $Value);
           $this->TestSchalter();
-          $this->ForwardData($JSONString);
+          $this->ForwardData();
           break;
 
         }
