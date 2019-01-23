56e9a598e171ab273344f23645db619a94f30a82
DonCri <36209368+DonCri@users.noreply.github.com>
2019-01-23 19:04:55 +0100

    Add files via upload

---
+++ eGateRohdaten/module.php
@@ -0,0 +1,58 @@
+<?
+    // Klassendefinition
+    class UTC extends IPSModule {
+ 
+        // Der Konstruktor des Moduls
+        // Überschreibt den Standard Kontruktor von IPS
+        public function __construct($InstanceID) {
+            // Diese Zeile nicht löschen
+            parent::__construct($InstanceID);
+ 
+	    // Selbsterstellter Code
+
+	   
+
+        }
+ 
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+		parent::Create();
+
+		 $this->RegisterVariableString("light", "eGate Werte Test", "", "0");
+		 $this->RegisterVariableString("temperatur", "Temperatur", "", "1");
+ 
+        }
+ 
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+        }
+ 
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MeineErsteEigeneFunktion() {
+            // Selbsterstellter Code
+        }
+
+
+	public function ReceiveData($JSONString) {
+ 
+	    // Empfangene Daten vom Gateway/Splitter
+	    $data = json_decode($JSONString);
+ 
+	    // Datenverarbeitung und schreiben der Werte in die Statusvariablen
+	    SetValue($this->GetIDForIdent("light"), print_r($data->Values, true));
+ 
+	}
+
+    }
+
+
+?>
